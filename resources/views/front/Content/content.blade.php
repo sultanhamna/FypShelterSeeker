954ae6f78726342a19662e45d8ebe0ef3f8ead29
hamna <hamnasultan220@gmail.com>
2024-12-20 20:56:00 +0500

    add

--- resources/views/front/Content/content.blade.php
+++ resources/views/front/Content/content.blade.php
@@ -1,6 +1,6 @@
 @extends('front.layout.master')
 @section('content')
-  <div class="hero-wrap bg-img" style="background-image: url('assets/front/images/bg.jpg');" data-stellar-background-ratio="0.5">
+  <div class="hero-wrap bg-img" style="background-image: url('assets/front/images/bg1.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="overlay-2"></div>
       <div class="container">
